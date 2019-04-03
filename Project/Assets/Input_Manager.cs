6ffd47e258fb9b2634dcfd6fca328c7e0208bc8f
Mikhail Albershteyn <HomakHaker@gmail.com>
2019-04-03 12:33:31 +0100

    sprites + basic funcionalidad (podemos quitarlo)

---
+++ Project/Assets/Input_Manager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Input_Manager : MonoBehaviour
+{
+
+    Player player;
+
+    float deltaTime;
+    float verInput, horInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Player>();
+        player.Init();
+    }
+
+    private void FixedUpdate()
+    {
+        getInput();
+        updatePlayerInputs();
+        deltaTime = Time.fixedDeltaTime;
+        player.tick(deltaTime);
+    }
+
+
+    private void getInput()
+    {
+        verInput = Input.GetAxis("Vertical");
+        horInput = Input.GetAxis("Horizontal");
+    }
+
+    private void updatePlayerInputs()
+    {
+        float magnitude = Mathf.Abs(verInput) + Mathf.Abs(horInput);
+        magnitude = Mathf.Clamp01(magnitude);
+        player.updateInputs(verInput, horInput, magnitude);
+    }
+
+
+}
---
+++ Project/Assets/Player.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // References
+    Rigidbody2D rb2d;
+
+
+
+    private float deltaTime;
+    private float verInput;
+    private float horInput;
+    [SerializeField] private float moveMagnitude;
+    [SerializeField] private States movementStates = States.idle;
+    [SerializeField] private float speed = 5f;
+
+
+    public void Init()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+
+
+
+    public void tick(float d)
+    {
+        deltaTime = d;
+        updateStates();
+        switch (movementStates)
+        {
+            case States.idle:
+
+            break;
+            case States.moving:
+                movement();
+            break;
+        }
+    }
+
+    private void movement()
+    {
+        //rb2d.AddForce(new Vector2(horInput, verInput) * speed);
+        rb2d.velocity = new Vector2(horInput, verInput) * speed;
+    }
+
+    private void updateStates()
+    {
+        movementStates = moveMagnitude > 0 ? States.moving : States.idle;
+    }
+
+    public void updateInputs(float ver, float hor, float magnitude)
+    {
+        verInput = ver;
+        horInput = hor;
+        moveMagnitude = magnitude;
+    }
+
+
+
+    enum States
+    {
+        idle,
+        moving
+    }
+}
