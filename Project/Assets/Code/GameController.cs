e8c9a7515649b9295394018bbed1d5c0b6277f5a
shmythor <shmythor@gmail.com>
2019-04-20 11:14:11 +0200

    GameController added

---
+++ Project/Assets/Code/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject fruitPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        createFruit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void createFruit() {
+        Instantiate(fruitPrefab, transform.position, Quaternion.identity);
+    }
+
+    
+}
