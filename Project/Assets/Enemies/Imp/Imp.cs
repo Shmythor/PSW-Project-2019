6d66f2fa2f649eb74d17782a157774602c92338a
Mikhail Albershteyn <HomakHaker@gmail.com>
2019-05-04 16:05:38 +0100

    Projectile still not done

--- Project/Assets/Enemies/Imp/Imp.cs
+++ Project/Assets/Enemies/Imp/Imp.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Imp : AEnemy, Iimp
 {
-
+    [Header("Projectile")]
     [SerializeField] private float projectileDamage;
+    [SerializeField] private float projectileSpeed;
     [SerializeField] private GameObject projectilePrefab;
 
     public void fireProjectile(Vector2 direction)
     {
-        Instantiate(projectilePrefab, transform.position, Quaternion.identity);
         Projectile projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity).GetComponent<Projectile>();
-        Debug.Log(projectile.ToString());
+        projectile.setDirection(direction);
         projectile.setDamage(projectileDamage);
     }
 }
--- Project/Assets/Enemies/Imp/Projectile.cs
+++ Project/Assets/Enemies/Imp/Projectile.cs
@@ -4,19 +4,53 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    
+
+    // References
+    private Rigidbody2D rb2d;
+    private Animator animator;
+
+
+    // General
     private float damage;
+    private float speed = 2f;
+    private Vector2 playerPosition;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        movement();
+        updateAnimations();
+    }
+
+
+    private void movement()
+    {
+        Vector2 direction = new Vector2(playerPosition.x - transform.position.x, playerPosition.y - transform.position.y);
+        direction.Normalize();
+        rb2d.velocity = direction * speed;
+    }
+
+
+    private void updateAnimations()
+    {
+        float magnitude = rb2d.velocity.y * rb2d.velocity.x;
+        Mathf.Clamp(magnitude, -1, 1);
+        animator.SetFloat("direction",magnitude);
+    }
+
+    public void setSpeed(float speed) { this.speed = speed; }
+    public void setDamage(float damage) { this.damage = damage; }
+    public void setDirection(Vector2 playerPosition) { this.playerPosition = playerPosition; }
 
     public void applyDamageToCharacter(ICharacter character)
     {
         character.reciveDamage(damage);
         Destroy(gameObject);
     }
-
-    private void Update()
-    {
-        
-    }
-
-
-    public void setDamage(float damage) { this.damage = damage; }
 }
